62f549852e98123ea31b00195bc9074a72288591
wyb <wangyinbo@jiayuan.com>
2019-09-26 09:49:21 +0800

    add

---
+++ NSOperationDemo/DownloadOperation.h
@@ -0,0 +1,17 @@
+//
+//  DownloadOperation.h
+//  NSOperationDemo
+//
+//  Created by wyb on 2019/9/26.
+//  Copyright © 2019 世纪佳缘. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DownloadOperation : NSOperation
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ NSOperationDemo/DownloadOperation.m
@@ -0,0 +1,13 @@
+//
+//  DownloadOperation.m
+//  NSOperationDemo
+//
+//  Created by wyb on 2019/9/26.
+//  Copyright © 2019 世纪佳缘. All rights reserved.
+//
+
+#import "DownloadOperation.h"
+
+@implementation DownloadOperation
+
+@end
